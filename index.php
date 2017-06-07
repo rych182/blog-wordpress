76ad0070244ee34356621431afd069b0a8f6cac4
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-06-07 15:41:05 -0500

    agregando mis archivos del blog de wordpress

---
+++ index.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Ric's blog</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700|Oswald" rel="stylesheet">
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="logo col-xs-12 col-sm-6">
+					<a href="#">
+						<img src="img/logo.png" alt="ricgc.com">
+					</a>
+				</div>
+				<div class="redes-sociales col-xs-12 col-sm-6">
+					<a class="twitter" href="https://twitter.com/Rych182">
+						<i class="fa fa-twitter-square"></i>
+					</a>
+					<a class="linkedin" href="https://www.linkedin.com/in/ricardo-garrido-a2a1b167/">
+						<i class="fa fa-linkedin-square"></i>
+					</a>
+					<a class="github" href="https://github.com/rych182">
+						<i class="fa fa-github-alt"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+			<nav class="menu">
+				<div class="container">
+					<div class="row">
+						<ul class="col-md-12">
+							<li><a href="#">HTML</a></li>
+							<li><a href="#">CSS</a></li>
+							<li><a href="#">Javascript</a></li>
+							<li><a href="#">JQuery</a></li>
+							<li><a href="#">Git</a></li>
+							<li><a href="#">PHP</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+
+			<div class="ad container hidden-xs">
+				<div class="row">
+					<div class="col-md-12">
+						<img src="img/ad.jpg" alt="Publicidad de Google">
+					</div>
+				</div>
+			</div>
+	</header>
+
+	<div class="container">
+		<div class="row">
+
+			<section class="main col-md-8">
+				<div class="row titulo-seccion">
+					<div class="col-md-12">
+						<h3>Lo mas reciente</h3>
+					</div>
+				</div>
+
+				<div class="row posts">
+					<article class="col-md-6 post">
+						<div class="contenedor">
+							<div class="thumb">
+								<a href="#">
+									<img src="img/1.jpg" width="700" alt="">
+								</a>
+							</div>
+							<div class="info">
+								<h3 class="titulo"><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</a></h3>
+								<p class="fecha">5 de Junio del 2017</p>
+								<p class="extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, accusamus et totam nihil distinctio voluptatem delectus possimus numquam tenetur natus!.</p>
+								<div class="categorias">
+									<a href="#">HTML</a>
+									<a href="#">CSS</a>	
+								</div>
+							</div>
+						</div>
+					</article>
+
+					<article class="col-md-6 post">
+						<div class="contenedor">
+							<div class="thumb">
+								<a href="#">
+									<img src="img/2.jpg" width="700" alt="">
+								</a>
+							</div>
+							<div class="info">
+								<h3 class="titulo"><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</a></h3>
+								<p class="fecha">5 de Junio del 2017</p>
+								<p class="extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, accusamus et totam nihil distinctio voluptatem delectus possimus numquam tenetur natus!.</p>
+								<div class="categorias">
+									<a href="#">HTML</a>
+									<a href="#">CSS</a>	
+								</div>
+							</div>
+						</div>
+					</article>
+
+					<article class="col-md-6 post">
+						<div class="contenedor">
+							<div class="thumb">
+								<a href="#">
+									<img src="img/3.jpg" width="700" alt="">
+								</a>
+							</div>
+							<div class="info">
+								<h3 class="titulo"><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</a></h3>
+								<p class="fecha">5 de Junio del 2017</p>
+								<p class="extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, accusamus et totam nihil distinctio voluptatem delectus possimus numquam tenetur natus!.</p>
+								<div class="categorias">
+									<a href="#">HTML</a>
+									<a href="#">CSS</a>	
+								</div>
+							</div>
+						</div>
+					</article>
+
+					<article class="col-md-6 post">
+						<div class="contenedor">
+							<div class="thumb">
+								<a href="#">
+									<img src="img/4.jpg" width="700" alt="">
+								</a>
+							</div>
+							<div class="info">
+								<h3 class="titulo"><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</a></h3>
+								<p class="fecha">5 de Junio del 2017</p>
+								<p class="extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, accusamus et totam nihil distinctio voluptatem delectus possimus numquam tenetur natus!.</p>
+								<div class="categorias">
+									<a href="#">HTML</a>
+									<a href="#">CSS</a>	
+								</div>
+							</div>
+						</div>
+					</article>
+
+					<article class="col-md-6 post">
+						<div class="contenedor">
+							<div class="thumb">
+								<a href="#">
+									<img src="img/5.jpg" width="700" alt="">
+								</a>
+							</div>
+							<div class="info">
+								<h3 class="titulo"><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</a></h3>
+								<p class="fecha">5 de Junio del 2017</p>
+								<p class="extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, accusamus et totam nihil distinctio voluptatem delectus possimus numquam tenetur natus!.</p>
+								<div class="categorias">
+									<a href="#">HTML</a>
+									<a href="#">CSS</a>	
+								</div>
+							</div>
+						</div>
+					</article>
+
+					<article class="col-md-6 post">
+						<div class="contenedor">
+							<div class="thumb">
+								<a href="#">
+									<img src="img/6.jpg" width="700" alt="">
+								</a>
+							</div>
+							<div class="info">
+								<h3 class="titulo"><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</a></h3>
+								<p class="fecha">5 de Junio del 2017</p>
+								<p class="extracto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, accusamus et totam nihil distinctio voluptatem delectus possimus numquam tenetur natus!.</p>
+								<div class="categorias">
+									<a href="#">HTML</a>
+									<a href="#">CSS</a>	
+								</div>
+							</div>
+						</div>
+					</article>
+
+				</div>
+				<section class="row paginacion">
+					<div class="col-md-12">
+						<ul>
+							<li class="disabled">Página 1 de 6</li>
+							<li><a href="#">1</a></li>
+							<li><a href="#">2</a></li>
+							<li><a href="#">3</a></li>
+							<li><a href="#">4</a></li>
+							<li><a href="#">5</a></li>
+							<li><a href="#">6</a></li>
+							<li><a href="#">Ultima</a></li>
+						</ul>
+					</div>
+				</section>
+
+			</section>
+
+			<aside class="sidebar col-md-4">
+				<div class="widget redes-sociales">
+					<div class="titulo-seccion">
+						<h3>Siguenos</h3>
+						<div class="redes-sociales">
+							<a class="twitter" href="https://twitter.com/Rych182">
+								<i class="icono fa fa-twitter-square"></i>
+								<span class="seguidores">200</span>
+							</a>
+							<a class="linkedin" href="https://www.linkedin.com/in/ricardo-garrido-a2a1b167/">
+								<i class="icono fa fa-linkedin-square"></i>
+								<span class="seguidores">100</span>
+							</a>
+							<a class="github" href="https://github.com/rych182">
+								<i class="icono fa fa-github-alt"></i>
+								<span class="seguidores">20</span>
+							</a>
+						</div>
+					</div>
+					
+				</div>
+				
+				<div class="widget boletin">
+					<div class="titulo-seccion">
+						<h3>Suscribete</h3>
+						<form action="" class="formulario">
+							<label for="email">Suscribete a nuestro boletin</label>
+							<input type="email" id="email" placeholder="Correo Electronico" required>
+							<input type="submit" value="Enviar">
+						</form>
+					</div>
+				</div>
+
+				<div class="widget ad">
+					<div class="contenedor-ad">
+						<a href="#">
+							<img src="img/ad2.jpg" alt="Anuncio">
+							<!--Puedo agregar mas imagenes, menús etc
+							Cuando esto se convierta a Wordpress 
+							vamos a poder decidir que widgets queremos
+							usar y cuales no -->
+						</a>
+					</div>
+				</div>
+
+			</aside>
+		</div>
+	</div>
+
+	<footer>
+		<div class="ad container hidden-xs">
+			<div class="row">
+				<div class="col-md-12">
+					<img src="img/ad.jpg" alt="">
+				</div>
+			</div>
+		</div>
+		
+		<nav class="menu">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<ul>
+							<li><a href="#">Inicio</a></li>
+							<li><a href="#">Acerca de</a></li>
+							<li><a href="#">Contacto</a></li>
+							<li><a href="#">Terminos y condiciones</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+
+		<div class="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<p>Sitio creado por <a href="http://ricgc.com">Ricardo Garrido</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
